a998c98dfffc0c40e34c96e6ad94d2e8b2902d14
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2020-08-04 16:52:44 +0430

    add example

---
+++ Projects/Phase04-CSharp/Example/CodeStarProject/Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DAL;
+
+namespace CodeStarProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var context = new Context();
+            context.Print();
+        }
+    }
+}
