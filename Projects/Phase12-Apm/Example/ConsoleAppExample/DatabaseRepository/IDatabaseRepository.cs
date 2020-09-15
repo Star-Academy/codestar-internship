dc107f6310e4635d25e9e7661c8b0e291ccf5a0f
Alireza Ziaee <alrz1999@gmail.com>
2020-09-15 12:32:17 +0430

    Example projects added

---
+++ Projects/Phase12-Apm/Example/ConsoleAppExample/DatabaseRepository/IDatabaseRepository.cs
@@ -0,0 +1,13 @@
+namespace ConsoleAppExample.DatabaseRepository
+{
+    public interface IDatabaseRepository
+    {
+        void Create();
+
+        void Read();
+
+        void Update();
+
+        void Delete();
+    }
+}
---
+++ Projects/Phase12-Apm/Example/ConsoleAppExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ConsoleAppExample.Businesses;
+using Elastic.Apm;
+
+namespace ConsoleAppExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number Of businesses to run");
+            var businessCount = int.Parse(Console.ReadLine());
+            var businessRepository = new BusinessRepository();
+            var rng = new Random();
+
+            for (int i = 0; i < businessCount; i++)
+            {
+                var businessType = rng.Next(1, 4);
+                string result = "";
+                switch (businessType)
+                {
+                    case 1:
+                        result = businessRepository.SmallBusiness();
+                        break;
+                    case 2:
+                        result = businessRepository.MediumBusiness();
+                        break;
+                    case 3:
+                        result = businessRepository.Longbusiness();
+                        break;
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
