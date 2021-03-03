5181ee709ea777be67abbf4bdd9dabb92312308c
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-03-03 11:01:00 +0330

    Add EfCore example

---
+++ Projects/Phase08-EFCore/EfCoreExample/EfCoreExample/Grade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EfCoreExample
+{
+    public class Grade
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public int StudentId { get; set; }
+
+        [ForeignKey("StudentId")]
+        public Student Student { get; set; }
+    }
+}
---
+++ Projects/Phase08-EFCore/EfCoreExample/EfCoreExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EfCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ExampleContext();
+
+            context.Database.EnsureCreated();
+
+            context.Students.Add(new Student() { LastName = "مالوردی", Name = "مهدی" });
+            context.Students.Add(new Student() { LastName = "عرب زاده", Name = "امیرحسین" });
+            context.Students.Add(new Student() { LastName = "کوششی", Name = "امیر مهدی" });
+            context.Students.Add(new Student() { LastName = "آقایی", Name = "حسین" });
+            context.Students.Add(new Student() { LastName = "آقایی", Name = "حسین" });
+
+            context.SaveChanges();
+
+            context.Grades.Add(new Grade() { Score = 20, StudentId = 1 });
+            context.Grades.Add(new Grade() { Score = 10, StudentId = 4 });
+            context.Grades.Add(new Grade() { Score = 19, StudentId = 3 });
+            context.Grades.Add(new Grade() { Score = -5, StudentId = 2 });
+
+            context.SaveChanges();
+
+            var student1 = context.Students.ToList();
+            var student2 = context.Students
+                .Join(context.Grades, s => s.Id, g => g.StudentId , (s,g) => g.Score)
+                .DefaultIfEmpty()
+                .ToList();
+        }
+    }
+}
